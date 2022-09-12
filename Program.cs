5c868963b3b19db1b2dfd372f02ec5195665db31
JetWaveDev <71261765+JetWaveDev@users.noreply.github.com>
2022-09-12 11:00:42 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Y2conApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
